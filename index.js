03bf1b357a813152afdbe8ee7ad67fd8b37ad857
Kartik <kartikbhalla12@gmail.com>
2020-05-18 20:05:14 +0530

    vildy: add cors package
    Signed-off-by: Kartik <kartikbhalla12@gmail.com>

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const cors = require('cors');
 
-require("./startup/logging")();
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
-require("./startup/prod")(app);
+app.use(cors());
+
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
-  console.log(`Listening on port: ${port}`)
+	console.log(`Listening on port: ${port}`)
 );
 
 module.exports = server;
